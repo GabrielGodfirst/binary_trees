9138b92cb5bbf75426650f1767bd96a70c70a997
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-02-28 07:11:02 +0000

    binary tree head

--- binary_trees.h
+++ binary_trees.h
@@ -18,7 +18,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+}binary_tree_t;
+
+typedef struct binary_tree_s binary_tree_t;
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
@@ -30,6 +32,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* other functions*/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
