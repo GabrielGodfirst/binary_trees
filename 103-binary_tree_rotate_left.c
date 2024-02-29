e26a8d2acd4e7cec4dd6b57db9a3025a83f99cff
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-02-29 10:27:40 +0000

    Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -2,33 +2,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Performs a left-rotation on a binary tree
- * @tree: Pointer to the root node of the tree to rotate
- * Return: Pointer to the new root node of the tree once rotated
+ * binary_tree_rotate_left - Performs a left rotation on a binary tree.
+ * @tree: Pointer to the root node of the tree to rotate.
+ * Return: Pointer to the new root node of the tree after rotation.
  */
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *new_root;
+
 	if (tree == NULL || tree->right == NULL)
-		return (NULL);
-
-	binary_tree_t *new_root = tree->right;
-
-	tree->right = new_root->left;
-
-	if (new_root->left != NULL)
-		new_root->left->parent = tree;
-
-	new_root->parent = tree->parent;
-	if (tree->parent != NULL)
 	{
-		if (tree->parent->left == tree)
-			tree->parent->left = new_root;
-		else
-			tree->parent->right = new_root;
+		return (NULL);
+	}
+
+	new_root = tree->right;
+	tree->right = new_root->left;
+	if (new_root->left != NULL)
+	{
+		new_root->left->parent = tree;
 	}
 
 	new_root->left = tree;
+	new_root->parent = tree->parent;
 	tree->parent = new_root;
 
 	return (new_root);
